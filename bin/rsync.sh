0ebf1ae08c7bad2409bbc6518db3a1fcd63c5a83
Markus Vuori <mvuori@opinsys.fi>
2012-09-10 13:21:54 +0300

    better logging

--- bin/rsync.sh
+++ bin/rsync.sh
@@ -3,10 +3,13 @@
 organization=$1
 host=$2
 
+logger "Rsyncing target ${host}.${organization} ..."
 rsync -zavx root@${host}.${organization}.opinsys.fi:/home /backup/$organization/$host &> /dev/null
 returncode=$?
 
 if (( $returncode != 0 )); then
      logger "RSYNC BACKUP process interrupted abnormally! host: ${host} organization: ${organization} return code: $returncode"
+else
+     logger "Rsync backup for target ${host}.${organization} finished OK."
 fi
 
