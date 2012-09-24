590149cf1a0ce43590df023bc897b51aae95436c
Markus Vuori <mvuori@opinsys.fi>
2012-09-24 09:55:47 +0300

    remade the reporting

--- usr/local/bin/end_backup.sh
+++ usr/local/bin/end_backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# This script lists first the running rsync processes and logs them as force killed before killing them
-ps xw | grep /usr/local/bin/rsync.sh | grep -v grep | sed 's/.*rsync.sh //' | awk "{print \$2,\$1,\"3 `date --rfc-3339=seconds`\"}" | sed 's/ /./'  >> /var/run/daily_backup_report
+ps xw | grep /usr/local/bin/rsync.sh | grep -v grep | sed 's/.*rsync.sh //' | awk "{print \$2,\$1,\"3 Failed: FORCE KILLED `date --rfc-3339=seconds`\"}" | sed 's/ /./'  >> /var/run/daily_backup_report
 
-sort /var/run/daily_backup_report | mail -s "Daily backup report" 'tuki@opinsys.fi'
+YRMON=$(date +%Y-%m)
+DAY=$(date +%Y-%m-%d-%a)
+DIR=/var/www/backup/report/$YRMON
+mkdir -p $DIR
+FILENAME=$DIR/report-$DAY
 
-pkill -f -USR1 rsync && sleep 5 && pkill -9 -f rsync
+sort /var/run/daily_backup_report | sed 's/ 1 $/ 1 UNKNOWN ERROR/' > $FILENAME
+cat $FILENAME | mail -s "Daily backup report" 'tuki@opinsys.fi'
+
+pkill -f rsync && sleep 3 && pkill -f rsync
 
 
 
