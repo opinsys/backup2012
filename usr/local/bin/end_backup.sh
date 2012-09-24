52257a251436cf8f68b5654e0b3feef65391ccb2
Markus Vuori <mvuori@opinsys.fi>
2012-09-24 09:58:49 +0300

    remade again reporting

--- usr/local/bin/end_backup.sh
+++ usr/local/bin/end_backup.sh
@@ -2,16 +2,10 @@
 
 ps xw | grep /usr/local/bin/rsync.sh | grep -v grep | sed 's/.*rsync.sh //' | awk "{print \$2,\$1,\"3 Failed: FORCE KILLED `date --rfc-3339=seconds`\"}" | sed 's/ /./'  >> /var/run/daily_backup_report
 
-YRMON=$(date +%Y-%m)
-DAY=$(date +%Y-%m-%d-%a)
-DIR=/var/www/backup/report/$YRMON
-mkdir -p $DIR
-FILENAME=$DIR/report-$DAY
-
-sort /var/run/daily_backup_report | sed 's/ 1 $/ 1 UNKNOWN ERROR/' > $FILENAME
-cat $FILENAME | mail -s "Daily backup report" 'tuki@opinsys.fi'
-
 pkill -f rsync && sleep 3 && pkill -f rsync
+
+/usr/local/bin/report.pl | mail -s "Daily backup report" 'mvuori@opinsys.fi'
+
 
 
 
---
+++ usr/local/bin/report.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl -w
+
+# This script formats html report and creates pie graphs
+use strict;
+use Data::Dumper;
+use GD::Graph::pie;
+
+my @data = `cat /var/run/daily_backup_report`;
+
+@data = sort @data;
+# How many slices we want (besides "OTHER")
+my $SLICES = 25;
+my $WWWDIR = '/var/www';
+my $BASEDIR = "backup/report";
+my $IMAGEDIR = "$BASEDIR/images";
+my $TEMPLATE = "$WWWDIR/$BASEDIR/.template";
+my @hosts = [];
+my @total_datas = [];
+my @transferred_datas = [];
+my $total_transferred = 0;
+my $total_data = 0;
+my $date = `date '+%Y.%m.%d'`;
+
+chomp $date;
+my @errors = ();
+my @images = ();
+my $transferred_first_slices = 0;
+my $total_first_slices = 0;
+foreach (@data) { 
+   push @errors, "<li class='error'>$1: Failed: FORCE KILLED</li>" if /^(\S+) 3.*force/i;
+   next unless / 1 /;
+   # If needed, we convert teras, gigas etc back to bytes
+   s/([\d\.]+)K/int($1*1024)/eg; 
+   s/([\d\.]+)M/int($1*(1024*1024))/eg; 
+   s/([\d\.]+)G/int($1*(1024*1024*1024))/eg; 
+   s/([\d\.]+)T/int($1*(1024*1024*1024*1024))/eg; 
+
+   chomp; 
+   # If we find size datas on this row, we save them for future use
+   if( /^(\S+) .*Total file size: ([\d\.]+).*ransferred file size: ([\d\.]+)/ ) {
+      my $host = $1;
+      my $total = $2;
+      my $transferred = $3;
+      $total_data += $total;
+      $total_transferred += $transferred;
+      push @hosts, $host;
+      push @total_datas, $total;
+      push @transferred_datas, $transferred;
+   }
+   else {
+      /^(\S+) 1 (.*)/;
+      my $host = $1;
+      my $desc = $2 || 'UNKNOWN ERROR (SSH, TIMEOUT, KEY?)';
+      push @errors, "<li class='error'>$host: $desc</li>";
+   }
+}
+my @report_template_array = `cat $TEMPLATE`;
+my $report_template = join("", @report_template_array);
+$report_template =~ s/ERRORLIST/join("\n", @errors)/e;
+$report_template =~ s/DATE/$date/ge;
+
+# As the GD::Graph wants it's data as an array of arrayrefs, we give him those
+
+# We want to sort hostnames (and total data amounts): "ORDER BY total data DESCENDING"
+# For that we create a helper array containing numbers in the right order and use that array for sorting.
+my @permutation = sort { $total_datas[$b] <=> $total_datas[$a] } (0..$#total_datas);
+my @hosts_according_to_total = (@hosts[@permutation])[0..$SLICES];
+@total_datas = (@total_datas[@permutation])[0..$SLICES];
+# We calculate the total data of first slices...
+foreach (@total_datas) { $total_first_slices += $_ };
+# So that we can push the remaining data for the "OTHER" slice
+push @total_datas, ( $total_data - $total_first_slices );
+push @hosts_according_to_total, "OTHER";
+
+# Again we want to sort hostnames (and transferred data amounts): "ORDER BY transferred data DESCENDING"
+@permutation = sort { $transferred_datas[$b] <=> $transferred_datas[$a] } (0..$#transferred_datas);
+my @hosts_according_to_transferred = (@hosts[@permutation])[0..$SLICES];
+@transferred_datas = (@transferred_datas[@permutation])[0..$SLICES];
+foreach (@transferred_datas) { $transferred_first_slices += $_ };
+push @transferred_datas, ( $total_transferred - $transferred_first_slices );
+push @hosts_according_to_transferred, "OTHER";
+
+
+# We draw a couple of pie charts and output them as PNG images.
+my $graph = GD::Graph::pie->new(800,800);
+my @gdata = ( \@hosts_according_to_total, \@total_datas );
+my $gd = $graph->plot(\@gdata);
+my $filename = "$IMAGEDIR/total_${date}.png";
+push @images, "<li><a class='graph' href='/$filename'><img class='thumb' src='/$filename' alt='Total data'></a></li>";
+open(IMG, ">$WWWDIR/$filename") or die$!;
+binmode IMG;
+print IMG $gd->png;
+close IMG;
+
+my $graph2 = GD::Graph::pie->new(800,800);
+my @gdata2 = ( \@hosts_according_to_transferred, \@transferred_datas );
+my $gd2 = $graph2->plot(\@gdata2);
+$filename = "$IMAGEDIR/transferred_${date}.png";
+push @images, "<li><a class='graph' href='/$filename'><img class='thumb' src='/$filename' alt='Transferred data'></a></li>";
+open(IMG, ">$WWWDIR/$filename") or die$!;
+binmode IMG;
+print IMG $gd2->png;
+close IMG;
+
+
+$report_template =~ s/GRAPHLIST/join("\n", @images)/e;
+$report_template =~ s/REPORT/join("", @data)/e;
+$filename = "$IMAGEDIR/transferred_${date}.png";
+my $report_url = "$BASEDIR/report_${date}.html";
+open(REPORT,">$WWWDIR/$report_url");
+print REPORT $report_template;
+close REPORT;
+print "http://10.246.133.5/$report_url";
